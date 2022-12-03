72b0b15d72674d2f1c3dfb7501847e65dda89454
ReeveFernandes <92554845+ReeveFernandes@users.noreply.github.com>
2022-12-03 18:51:16 -0500

    removed unnecessary comments

--- functions/twosComplement.vhdl
+++ functions/twosComplement.vhdl
@@ -1,35 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 12/03/2022 04:53:31 PM
--- Design Name: 
--- Module Name: twoscompliment - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity twoscompliment is
     Port ( xin : in STD_LOGIC_VECTOR (3 downto 0);
