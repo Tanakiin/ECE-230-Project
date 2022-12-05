f2a4a91c9ec2ee984a63b8ac8aadefdded48a1c4
vergowv1 <119555652+vergowv1@users.noreply.github.com>
2022-12-04 23:32:14 -0500

    Create comparator.vhdl

---
+++ functions/comparator.vhdl
@@ -0,0 +1,22 @@
+entity compare is
+    Port ( x : in STD_LOGIC_VECTOR (3 downto 0);
+           y : in STD_LOGIC_VECTOR (3 downto 0);
+           result : out STD_LOGIC_VECTOR (3 downto 0);
+           flag : out STD_LOGIC);
+end compare;
+
+architecture Behavioral of compare is
+
+begin
+process(x,y)
+begin
+if (x >= y) then
+result <= "1111";
+flag <= '1';
+else
+result <= "0000";
+flag <= '0';
+
+end if;
+end process;
+end Behavioral;
